361ca8b3fb5e113c6af01b48883a0cb7b2ae238a
adwaith <adwaith1990@gmail.com>
2016-09-17 22:00:05 -0700

    Validation and test evaluations are giving bad_alloc error. will try with feed_dicts instead

---
+++ A4/convnet_pooling.py
@@ -0,0 +1,133 @@
+from __future__ import print_function
+import numpy as np
+import tensorflow as tf
+from six.moves import cPickle as pickle
+from six.moves import range
+from sklearn.preprocessing import OneHotEncoder
+pickle_file='../notMNIST.pickle'
+
+'''
+print("Reading the pickled file...")
+datasets=[]
+with (open("../notMNIST.pickle","rb")) as openfile:
+	while True:
+		try:
+			datasets.append(pickle.load(openfile))
+		except EOFError:
+			break
+'''
+with open(pickle_file,'rb') as f:
+	save=pickle.load(f)
+	train_dataset=save['train_dataset']
+	train_labels=save['train_labels']
+	valid_dataset=save['valid_dataset']
+	valid_labels=save['valid_labels']
+	test_dataset=save['test_dataset']
+	test_labels=save['test_labels']
+	del save
+	
+
+image_size=28
+num_channels=1
+train_dataset=train_dataset.reshape(-1,image_size,image_size,num_channels).astype(np.float32)
+test_dataset=test_dataset.reshape(-1,image_size,image_size,num_channels).astype(np.float32)
+valid_dataset=valid_dataset.reshape(-1,image_size,image_size,num_channels).astype(np.float32)
+
+print("One hot encoding the class labels...")
+#One hot encoding the data
+enc=OneHotEncoder()
+train_labels=enc.fit_transform(train_labels.reshape(-1,1)).toarray()
+test_labels=enc.fit_transform(test_labels.reshape(-1,1)).toarray()
+valid_labels=enc.fit_transform(valid_labels.reshape(-1,1)).toarray()
+
+'''
+print(X_train.shape,y_train.shape)
+print(X_val.shape,y_val.shape)
+print(X_test.shape,y_test.shape)
+'''
+batch_size = 16
+patch_size = 5
+depth = 16
+num_hidden = 64
+num_labels=10
+def accuracy(predictions, labels):
+  return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
+          / predictions.shape[0])
+
+graph = tf.Graph()
+
+with graph.as_default():
+
+  # Input data.
+  tf_train_dataset = tf.placeholder(
+    tf.float32, shape=(batch_size, image_size, image_size, num_channels))
+  tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+  tf_valid_dataset = tf.constant(valid_dataset)
+  tf_test_dataset = tf.constant(test_dataset)
+  
+  # Variables.
+  layer1_weights = tf.Variable(tf.truncated_normal(
+      [patch_size, patch_size, num_channels, depth], stddev=0.1))
+  layer1_biases = tf.Variable(tf.zeros([depth]))
+  layer2_weights = tf.Variable(tf.truncated_normal(
+      [patch_size, patch_size, depth, depth], stddev=0.1))
+  layer2_biases = tf.Variable(tf.constant(1.0, shape=[depth]))
+  layer3_weights = tf.Variable(tf.truncated_normal(
+      [image_size // 4 * image_size // 4 * depth, num_hidden], stddev=0.1))
+  layer3_biases = tf.Variable(tf.constant(1.0, shape=[num_hidden]))
+  layer4_weights = tf.Variable(tf.truncated_normal(
+      [num_hidden, num_labels], stddev=0.1))
+  layer4_biases = tf.Variable(tf.constant(1.0, shape=[num_labels]))
+  
+  # Model.
+  def model(data):
+    conv1 = tf.nn.conv2d(data, layer1_weights,strides=[1,1,1,1],padding='SAME')
+    hidden = tf.nn.relu(conv1 + layer1_biases)
+    hidden_pool1=tf.nn.max_pool(hidden,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
+    print("conv layer 1 - ",hidden_pool1.get_shape().as_list())
+    conv2 = tf.nn.conv2d(hidden_pool1, layer2_weights,strides=[1,1,1,1],padding='SAME')
+    hidden = tf.nn.relu(conv2 + layer2_biases)
+    hidden_pool2=tf.nn.max_pool(hidden,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
+    print("conv layer 1 - ",hidden_pool2.get_shape().as_list())
+    shape = hidden_pool2.get_shape().as_list()
+    reshape = tf.reshape(hidden_pool2, [shape[0], shape[1] * shape[2] * shape[3]])
+    hidden = tf.nn.relu(tf.matmul(reshape, layer3_weights) + layer3_biases)
+    print("fully connected layer - ",hidden.get_shape().as_list())
+    return tf.matmul(hidden, layer4_weights) + layer4_biases
+  
+  # Training computation.
+  logits = model(tf_train_dataset)
+  loss = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels))
+    
+  # Optimizer.
+  optimizer = tf.train.GradientDescentOptimizer(0.05).minimize(loss)
+  
+  # Predictions for the training, validation, and test data.
+  train_prediction = tf.nn.softmax(logits)
+  valid_prediction = tf.nn.softmax(model(tf_valid_dataset))
+  test_prediction = tf.nn.softmax(model(tf_test_dataset))
+
+num_steps=201
+with tf.Session(graph=graph) as session:
+  tf.initialize_all_variables().run()
+  print('Initialized')
+  for step in range(num_steps):
+    offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+    batch_data = train_dataset[offset:(offset + batch_size), :, :, :]
+    batch_labels = train_labels[offset:(offset + batch_size), :]
+    feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
+    _, l, predictions = session.run(
+      [optimizer, loss, train_prediction], feed_dict=feed_dict)
+    if (step % 50 == 0):
+      print('Minibatch loss at step %d: %f' % (step, l))
+      print('Minibatch accuracy: %.1f%%' % accuracy(predictions, batch_labels))
+      
+      print('Validation accuracy: %.1f%%' % accuracy(
+        valid_prediction.eval(), valid_labels))
+  print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
+	
+
+
+
+
