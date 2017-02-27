9974119ce9767d491f46caf47b19346dc13be435
adwaith <adwaith1990@gmail.com>
2017-02-27 14:49:53 -0800

    achaar.py now saves number of encoded labels for each target legth or digit

--- capstone/achaar.py
+++ capstone/achaar.py
@@ -6,7 +6,7 @@ from scipy.ndimage.filters import uniform_filter
 from sklearn.preprocessing import OneHotEncoder
 
 total_images=33401
-images_used=8000
+images_used=80
 width=128
 height=64
 image_path='train_mod_'+str(width)+'x'+str(height)+'/'
@@ -20,15 +20,17 @@ for i in range(images_used):
 	if length>5:
 		length=5
 	lengths[i]=length
-	
+	#print("image %s",i)
 	for j in range(length):
 		digit=struct['digitStruct']['bbox'][0][i]['label'][0][j][0][0]
+		#print(digit)
 		if digit>9:
-			digits[i][j]=0.
+			digits[i][j]=int(0)
+			
 		else:
 			digits[i][j]=digit
-
-
+	#print("----")
+print("Reading images from the v7.mat format done")
 digit1=np.empty(images_used)
 digit2=np.empty(images_used)
 digit3=np.empty(images_used)
@@ -43,6 +45,7 @@ for i in range(images_used):
 	digit5[i]=digits[i][4]
 
 target=[lengths,digit1,digit2,digit3,digit4,digit5]
+
 dataset=[]
 
 for i in range(images_used):
@@ -50,6 +53,7 @@ for i in range(images_used):
 	image=Image.open(image_path+image_name)
 	dataset.append(np.array(image))
 
+print("Dataset of images created")
 def window_stdev(arr,radius):
 	c1=uniform_filter(arr,radius*2,mode='constant',origin=-radius)
 	c2=uniform_filter(arr*arr,radius*2,mode='constant',origin=-radius)
@@ -57,30 +61,29 @@ def window_stdev(arr,radius):
 
 mean=np.mean(dataset)
 stddev=np.std(dataset)
-
+print("Standard deviation of dataset calculated")
 #stddev=window_stdev((np.asarray(dataset)).reshape(images_used*width*height*3),20)	
 #print((np.asarray(dataset)).reshape(images_used*width*height*3))
 print('standard deviation=',stddev)
 
 dataset=.5*(dataset-mean)/stddev
 
-training_samples=7000
+training_samples=int(images_used*0.9)
 train_target=[]
 test_target=[]
+all_labels=[]
 train_dataset=dataset[0:training_samples]
 test_dataset=dataset[training_samples:total_images]
-print("One hot encoding the labels...")
+
 for i in range(6):
 	enc=OneHotEncoder()
 	target_enc=enc.fit_transform(target[i].reshape(-1,1)).toarray()	
 	train_target.append(target_enc[0:training_samples])
 	test_target.append(target_enc[training_samples:total_images])
-
-print(train_dataset.shape)
-print(train_target[0].shape)
-print(test_dataset.shape)
-print(test_target[0].shape)
-
+	all_labels.append(target_enc[0].size)
+	
+print(all_labels)
+print("One hot encoding the labels done.")
 achaar_file = 'svhn.pickle'
 try:
   f = open(achaar_file, 'wb')
@@ -89,9 +92,12 @@ try:
     'train_target': train_target,
 	'test_dataset': test_dataset,
     'test_target': test_target,
+	'all_labels': all_labels,
     }
   pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
   f.close()
 except Exception as e:
   print('Unable to save data to', achaar_file, ':', e)
   raise
+
+print("Achaarifying of images done.")
